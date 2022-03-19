6f6c8f26cb892733fce09c73ee0308942065fba3
MissCrispenCakes <10444589+MissCrispenCakes@users.noreply.github.com>
2022-03-19 00:42:25 -0400

    Update models.js

--- docs/website/pages/mini-game/js/models.js
+++ docs/website/pages/mini-game/js/models.js
@@ -4,11 +4,10 @@ var characters = [],
 function ARModel(name, dialogue) {
     this.name = name;
     this.dialogue = dialogue;
-
 }
 
 ARModel.prototype.speak = function() {
-    return this.dialogue;   
+    return this.dialogue;
 }
 
 
